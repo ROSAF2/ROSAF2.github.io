c2a762a2686ffec606b1d5a62991739120e0f2df
Francisco Rosas <rosaf2@student.op.ac.nz>
2020-05-04 12:54:52 +1200

    Contact Form Updated

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,30 +1,30 @@
 
 $(_ => {
     //Contact Form
-    document.querySelector(".button.contactPage").addEventListener("click", element => {
+    const form = document.querySelector("form");
+    const name = document.getElementById("form-name");
+    const email = document.getElementById("email");
+    const phone = document.getElementById("phone");
+    const feedbackDiv = document.getElementById("feedback");
+    
+    form.addEventListener("submit", element => {
         element.preventDefault();
-        
-        let name = document.getElementById("form-name").value;
-        let email = document.getElementById("email").value;
-        let phone = document.getElementById("phone").value;
-        
-        if(name !== "" && email !== "" && phone !== "" ){
-            //Deleting div container contents
-            document.getElementById("feedback").innerHTML = "";
-            //Creating and appending message
-            let h1 = document.createElement("h1");
-            h1.innerText = "Thank you for your message!";
-            document.getElementById("feedback").append(h1);
-            //Clearing Form Entries
-            document.getElementById("form-name").value ="";
-            document.getElementById("email").value ="";
-            document.getElementById("phone").value ="";
-         }else{
-             alert("Fill all the fields.");
-         }
+        DisplayMessage();
+        //Clearing Form Entries
+        name.value ="";
+        email.value ="";
+        phone.value ="";
     });
+    function DisplayMessage(){
+        //Deleting div container contents
+        feedbackDiv.innerHTML = "";
+        //Creating and appending message
+        let h1 = document.createElement("h1");
+        h1.innerText = "Thank you for your message!";
+        feedbackDiv.append(h1);
+    }
     //End
-    });
+});
     
     
     
